83fe07e5c369843e59f9d812f7606d4842205e89
vchinsomboon <varalee_c@hotmail.com>
2024-07-15 23:28:49 -0700

    temporary hide unfinished project

--- src/config.js
+++ src/config.js
@@ -1,23 +1,25 @@
 const config = {
-  API_BASE_URL: process.env.NODE_ENV === 'production'
-    ? 'https://flask-react-portfolio.uw.r.appspot.com/'
-    : 'http://127.0.0.1:5000',
-  EMAIL_URL: 'varalee.c7@gmail.com',
-  GITHUB_URL: 'https://github.com/vchinsomboon',
-  LINKEDIN_URL: 'https://www.linkedin.com/in/varalee-chinsomboon-a12a1215b',
+  API_BASE_URL:
+    process.env.NODE_ENV === "production"
+      ? "https://flask-react-portfolio.uw.r.appspot.com/"
+      : "http://127.0.0.1:5000",
+  EMAIL_URL: "varalee.c7@gmail.com",
+  GITHUB_URL: "https://github.com/vchinsomboon",
+  LINKEDIN_URL: "https://www.linkedin.com/in/varalee-chinsomboon-a12a1215b",
   PROJECTS: [
     {
-      id: 'Project1',
-      title: 'Titanic Survival (ML)',
-      description: 'Predicting survival on the Titanic using machine learning techniques.'
+      id: "Project1",
+      title: "Titanic Survival (ML)",
+      description:
+        "Predicting survival on the Titanic using machine learning techniques.",
     },
-    {
-      id: 'Project2',
-      title: 'Project 2 (TBD)',
-      description: 'Description example...'
-    },
+    // {
+    //   id: "Project2",
+    //   title: "Project 2 (TBD)",
+    //   description: "Project 2 Description",
+    // },
     // Add more projects as needed
-  ]
+  ],
 };
 
 export default config;
